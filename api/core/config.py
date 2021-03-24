f9c139ac5152ad8b75752f87c797f9c2b54f826d
quebim <kevinledesmam95@gmail.com>
2021-03-24 01:58:37 -0300

    CRUD Merge

--- api/core/config.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseSettings
-
-
-class Settings(BaseSettings):
-    SQLALCHEMY_DATABASE_URL: str = "sqlite:///./sql_app.db"
-
-
-settings = Settings()
--- api/models/product.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Integer, String, Column, Float
-from api.db.base_class import Base
-
-
-class Product(Base):
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True)
-    price = Column(Float)
-    stock = Column(Integer)
