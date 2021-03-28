7bec960e6927bbf41e75f29e2bd6e49859440b5c
quebim <kevinledesmam95@gmail.com>
2021-03-28 18:39:41 -0300

    Components added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
+
+import React, {useState} from 'react';
+import RegistrationForm from './components/RegistrationForm/RegistrationForm';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
 
 function App() {
+  const [title, updateTitle] = useState(null);
+  const [errorMessage, updateErrorMessage] = useState(null);
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header title={title}/>
+        <div className="container d-flex align-items-center flex-column">
+          <Switch>
+            <Route path="/" exact={true}>
+              <RegistrationForm showError={updateErrorMessage} updateTitle={updateTitle}/>
+            </Route>
+            <Route path="/register">
+              <RegistrationForm showError={updateErrorMessage} updateTitle={updateTitle}/>
+            </Route>
+
+          </Switch>
+        </div>
     </div>
+    </Router>
   );
 }
 
---
+++ frontend/src/constants/apiConstants.js
@@ -0,0 +1,2 @@
+export const API_BASE_URL = "127.0.0.1:58200";
+export const ACCESS_TOKEN_NAME = 'login_access_token'; 
